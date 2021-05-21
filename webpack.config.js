25d4d34ba2e95cf431fa85abf28643cffc68e155
LordAldi <aldianugra09@gmail.com>
2021-05-22 02:34:32 +0700

    add sass loader, overwrite bootstrap color

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 module.exports = {
   mode: "development",
-  devtool: "source-map",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
@@ -10,8 +9,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Inject css to DOM
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
