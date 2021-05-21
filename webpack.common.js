619d2262ad12e206de0180ff92d8bff40124b03b
LordAldi <aldianugra09@gmail.com>
2021-05-22 04:06:29 +0700

    Add html-loader, file-loader, and clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -21,6 +21,20 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "img",
+          },
+        },
+      },
     ],
   },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,12 @@
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new CleanWebpackPlugin()],
 });
